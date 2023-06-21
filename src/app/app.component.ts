8e40ec1d6fd0840272341a7fbeb5a7c787feed70
carloschg1 <aluher5242@ieselcaminas.org>
2023-06-21 13:06:47 +0200

    3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,33 @@
-import { Component,OnInit } from '@angular/core';
-import{FormBuilder,FormGroup,Validators} from '@angular/forms';
-import{TareaService} from './Services/tarea.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TareaService } from './Services/tarea.service';
 import { Tarea } from './Interfaces/tarea';
-import { error } from 'console';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-
-  listaTareas:Tarea[]=[];
-  formularioTarea:FormGroup;
+export class AppComponent implements OnInit {
+  listaTareas: Tarea[] = [];
+  formularioTarea: FormGroup;
 
   constructor(
-    private fb:FormBuilder,
-    private tareaService:TareaService
-    ){
-
-      this.formularioTarea=this.fb.group({
-        nombre:['',Validators.required]
+    private fb: FormBuilder,
+    private tareaService: TareaService
+  ) {
+    this.formularioTarea = this.fb.group({
+      nombre: ['', Validators.required]
     });
   }
 
-  obtenerTareas(){
+  obtenerTareas() {
     this.tareaService.getList().subscribe({
-      next:(result)=>{
-        this.listaTareas=result;
+      next: (result) => {
+        this.listaTareas = result;
+      },
+      error: (err) => {
+        // Manejar el error
       }
     });
   }
@@ -37,24 +36,35 @@ export class AppComponent implements OnInit{
     this.obtenerTareas();
   }
 
-  agregarTarea(){
-    const request:Tarea={
-      idTarea:0,
-      nombreTarea: this.formularioTarea.value.nombre
+  agregarTarea() {
+    if (this.formularioTarea.invalid) {
+      return; // Si el formulario es inválido, no se agrega la tarea
     }
 
+    const request: Tarea = {
+      idTarea: 0,
+      nombreTarea: this.formularioTarea.value.nombre
+    };
+
     this.tareaService.add(request).subscribe({
-      next:(result)=>{
+      next: (result) => {
         this.listaTareas.push(result);
-        this.formularioTarea.patchValue({nombre:''});
-      },error:(err)=>{}});
+        this.formularioTarea.patchValue({ nombre: '' });
+      },
+      error: (err) => {
+        // Manejar el error
+      }
+    });
   }
 
-  eliminarTarea(Tarea:Tarea){
-    this.tareaService.delete(Tarea.idTarea).subscribe({
-      next:(result)=>{
-        const nuevaLista = this.listaTareas.filter( item => item.idTarea !== Tarea.idTarea);
-        this.listaTareas = nuevaLista;
-      },error:(err)=>{}});
+  eliminarTarea(tarea: Tarea) {
+    this.tareaService.delete(tarea.idTarea).subscribe({
+      next: (result) => {
+        this.listaTareas = this.listaTareas.filter(item => item.idTarea !== tarea.idTarea);
+      },
+      error: (err) => {
+        // Manejar el error
+      }
+    });
   }
 }
